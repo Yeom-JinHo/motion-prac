a9e84f54948e72d660db6d5ee067305176b8c821
Yeom-JinHo <pwlsghq@naver.com>
2022-08-20 18:33:40 +0900

    feat : 이미지 컴포추가

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
-import { PageComponent } from "./components/page.js";
+import { ImageComponent } from "./page/item/image.js";
+import { PageComponent } from "./page/page.js";
 
 class App {
   private readonly page: PageComponent;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attatchTo(appRoot);
+
+    const image = new ImageComponent(
+      "Image title",
+      "https://picsum.photos/600/300"
+    );
+    image.attatchTo(appRoot, "beforeend");
   }
 }
 
